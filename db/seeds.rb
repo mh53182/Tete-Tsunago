c646e99ced2bdfa742c67f0787fc457481a0cbbd
mh53182 <marie.i.077145@gmail.com>
2023-12-25 19:07:38 +0000

    [fix]seedファイル更新

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,11 +6,11 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-# Admin.create!(
-#    email: 'tete@tsunago',
-#    password: 'aaaaaa'
-# )
+Admin.create!(
+    email: 'tete@tsunago',
+    password: 'aaaaaa'
+)
 
-Admin.find_or_create_by!(email: ENV['ADMIN_EMAIL']) do |admin|
- admin.password = ENV['ADMIN_PASSWORD']
-end
+# Admin.find_or_create_by!(email: ENV['ADMIN_EMAIL']) do |admin|
+# admin.password = ENV['ADMIN_PASSWORD']
+# end
