f06d830d402ce5327f72ad5b1cfb027483e06cbb
mh53182 <marie.i.077145@gmail.com>
2023-12-26 02:35:08 +0000

    [add]誕生日メッセージ

--- app/controllers/public/sessions_controller.rb
+++ app/controllers/public/sessions_controller.rb
@@ -26,12 +26,22 @@ class Public::SessionsController < Devise::SessionsController
   #   devise_parameter_sanitizer.permit(:sign_in, keys: [:attribute])
   # end
 
+  # sessionのcreateをオーバーライド
+  # 誕生日判定メソッドをbefore_actionで使うとフラッシュメッセージが上書きされるため、誕生日判定をログイン後に行う
+  def create
+    super do |resource|
+      check_birthdays if resource.persisted?
+    end
+  end
+
   # ゲストログイン
   def guest_sign_in
     user = User.guest
     sign_in user
     redirect_to user_path(user), notice: "ゲストユーザーとしてログインしました"
   end
+
+  private
 
   # 退会済アカウントのログイン不可処理
   def user_state
@@ -41,6 +51,29 @@ class Public::SessionsController < Devise::SessionsController
     return if user.is_active
     flash[:alert] = "すでに退会されたアカウントです。別のメールアドレスをお使いください。"
     redirect_to new_user_session_path
+  end
+
+  # お子様の誕生日判定
+  def check_birthdays
+    
+    # ユーザーに紐づけられたお子様がいるかどうかを確認
+    return if current_user.children.blank?
+
+    current_user.children.each do |child|
+      
+      # お子様の誕生日が設定されているか確認
+      next if child.birthday.blank?
+
+      # お子様の誕生日と今日の日付が一致するか確認
+      if child.birthday.month == Date.today.month && child.birthday.day == Date.today.day
+        
+        # ログイン成功のフラッシュメッセージを保持
+        existing_notice = flash[:notice].to_s
+        
+        # ログイン成功と誕生日のメッセージを作成
+        flash[:notice] = "#{existing_notice}　#{child.nickname} のお誕生日 おめでとうございます！"
+      end
+    end
   end
 
 end
